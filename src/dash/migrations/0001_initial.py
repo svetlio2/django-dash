b757b0c5ab5ff02e0f1acad21c221a47b9aff70f
svetlio <svetlio1994@gmail.com>
2017-03-16 11:25:51 +0200

    Remove migration dependency to latest auth migration.

--- src/dash/migrations/0001_initial.py
+++ src/dash/migrations/0001_initial.py
@@ -11,7 +11,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('auth', '__latest__'),
     ]
 
     operations = [
